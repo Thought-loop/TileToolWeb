3054f04fd3678d2b2eea564775106783ba2aac7d
Nick Steinhouse <nick.steinhouse@gmail.com>
2022-02-22 13:34:43 -0500

    initial commit with springboot initilizer stuff only

---
+++ src/main/java/com/thought_loop/TileToolWeb/TileToolWebApplication.java
@@ -0,0 +1,13 @@
+package com.thought_loop.TileToolWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TileToolWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TileToolWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thought_loop/TileToolWeb/TileToolWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thought_loop.TileToolWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TileToolWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
